3f457b4225424bed94355db06e62317ae0501231
Santiago397 <santilopez397@gmail.com>
2022-01-26 19:41:51 -0500

    added styles to fix the card's directions

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,6 +12,9 @@ function Index() {
                 <ProductCard img={aguila} name='Aguila' qLikes='100' qComments='45' qViews='200' />
                 <ProductCard img={poker} name='Poker' qLikes='300' qComments='145' qViews='500' />
                 <ProductCard img={costenia} name='Costeña' qLikes='200' qComments='115' qViews='260' />
+                <ProductCard img={costenia} name='Costeña' qLikes='200' qComments='115' qViews='260' />
+                <ProductCard img={costenia} name='Costeña' qLikes='200' qComments='115' qViews='260' />
+                <ProductCard img={costenia} name='Costeña' qLikes='200' qComments='115' qViews='260' />
             </ul>
         </section>
     );
